14500556a0b59d27a6d3018b0503c2855f0d4889
yazidbky <yazid.bekkai@univ-msila.dz>
2024-12-10 02:10:00 +0100

    test

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -15,7 +15,7 @@ export const getUserById = async (req, res) => {
     const user = await User.findById(id);
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "User not found?" });
     }
 
     res.status(200).json(user);
